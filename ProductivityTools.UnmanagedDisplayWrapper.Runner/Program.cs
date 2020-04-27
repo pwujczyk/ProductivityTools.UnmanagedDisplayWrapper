575416829f617a9efe3d648fb6a07b490b354ee1
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-04-27 22:36:20 +0200

    dev mode

---
+++ ProductivityTools.UnmanagedDisplayWrapper.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductivityTools.UnmanagedDisplayWrapper.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
